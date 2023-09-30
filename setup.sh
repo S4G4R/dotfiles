af6d14a267c46bf8be0411f9d877ccb1aa81f01b
Sagar Vrajalal <sagar.vrajalal@unifize.com>
2023-09-30 17:12:20 +0530

    add setup file

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# Download oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ] ; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi    
+
+# Download p10k theme
+if [ ! -d "$HOME/powerlevel10k" ] ; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi    
+
+# Download asdf version manager
+if [ ! -d "$HOME/.asdf" ] ; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+fi    
+
+# Add asdf plugins
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf plugin-add lein https://github.com/miorimmax/asdf-lein.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+
+# Install runtimes
+asdf install
