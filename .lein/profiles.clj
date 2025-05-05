030408d9c7784d324833363eeeb59b2dfd71433c
Sagar Vrajalal <sagarvrajalal@gmail.com>
2025-05-05 23:23:16 +0530

    Add clojure config

---
+++ .lein/profiles.clj
@@ -0,0 +1,19 @@
+{:user {:plugins [[lein-monolith "1.10.0"]
+                  [cider/cider-nrepl "0.55.7"]
+                  [org.clojars.timvisher/lein-checkout "0.4.2"]]
+        :checkout {:search-roots ["/home/sagar/unifize-server/libs"]}
+        ;; :jvm-opts ["-Djava.net.preferIPv4Addresses=true"]
+	}
+ :bench {:jvm-opts ["-Djdk.attach.allowAttachSelf"]
+         :dependencies [[com.clojure-goes-fast/clj-memory-meter "0.3.0"]
+                        [criterium "0.4.6"]]}
+ ;; lein with-profile +otel repl (api server tracing)
+ :otel {:jvm-opts ["-javaagent:opentelemetry-javaagent.jar"
+                   "-Dotel.resource.attributes=service.name=api-sagar"
+                   "-Dotel.metrics.exporter=none"
+                   "-Dotel.logs.exporter=none"
+	           ;; For Lightstep developer satellite
+                   "-Dotel.exporter.otlp.endpoint=http://localhost:8360"
+                   ;; For Jaeger
+                   ;; "-Dotel.exporter.otlp.endpoint=http://localhost:4317"
+]}}
